a3681b389c90ebd31fae87f6dce0264af761c19d
jhyeokqq <jinmb4136@gmail.com>
2024-03-06 22:28:33 +0900

    commit

--- mall/admin.py
+++ mall/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Category, Product, Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "total_amount", "status"]
+    actions = ["make_cancel", "update"]
+
+    @admin.display(description=f"지정 주문결제를 취소합니다.")
+    def make_cancel(self, request, queryset):
+        for order in queryset:
+            order.cancel("관리자가 주문결제를 취소했습니다.")
+        self.message_user(request, f"{queryset.count()}개의 주문결제를 취소했습니다.")
+
+    @admin.display(description="지정 주문의 결제상황을 업데이트합니다.")
+    def update(self, request, queryset):
+        for order in queryset:
+            order.update()
+        self.message_user(request, f"{queryset.count()}개의 결제상황을 업데이트했습니다.")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name"]
+    list_display_links = ["name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["category", "name", "price", "status"]
+    list_display_links = ["name"]
+    list_filter = ["category", "status", "created_at", "updated_at"]
+    date_hierarchy = "updated_at"
+    actions = ["make_active"]
+
+    @admin.display(description=f"지정 상품을 {Product.Status.ACTIVE.label} 상태로 변경합니다.")
+    def make_active(self, request, queryset):
+        count = queryset.update(status=Product.Status.ACTIVE)
+        self.message_user(
+            request, f"{count}개의 상품을 {Product.Status.ACTIVE.label} 상태로 변경했습니다."
+        )
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,16 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
-import environ  # django-environ
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # django-environ
-# https://django-environ.readthedocs.io/en/latest/install.html
-
 env = environ.Env()
 
 env_path = BASE_DIR / ".env"
@@ -35,40 +31,34 @@ if env_path.exists():
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str(
     "SECRET_KEY",
-    default="django-insecure-#svfi28q100b!yri$a!!w2ewj3)%u%71^^vx#*7-277*^it+9z",
+    default="django-insecure-%%m@r57!sbz8w$_(p#so+-_!q6fyb!or$_wj@l2ur-4nv62_v_",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)  # 1, t, true
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    # django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.humanize",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # third apps
+    "django.contrib.humanize",
     "debug_toolbar",
     "django_bootstrap5",
     "sorl.thumbnail",
-    "widget_tweaks",
-    # local apps
     "accounts",
     "mall",
     "mall_test",
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -76,6 +66,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -103,7 +94,6 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 
 DATABASES = {
     "default": env.db("DATABASE_URL", default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
@@ -149,13 +139,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = env.str("STATIC_ROOT", default=BASE_DIR / "staticfiles")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = env.str("MEDIA_ROOT", default=BASE_DIR / "mediafiles")
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -164,9 +150,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # django-debug-toolbar
-
 INTERNAL_IPS = env.list("INTERNAL_IPS", default=["127.0.0.1"])
-
 
 # 포트원
 PORTONE_SHOP_ID = env.str("PORTONE_SHOP_ID", default="")
